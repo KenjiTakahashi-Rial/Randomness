cf05acd076bd1c455a0506d173580510fe0444a5
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-10-31 15:08:50 -0700

    Added MainMenuActivity and layout

---
+++ app/src/main/java/com/kenjitakahashirial/randomness/MainMenuActivity.kt
@@ -0,0 +1,29 @@
+package com.kenjitakahashirial.randomness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainMenuActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerViewAdapter: RecyclerView.Adapter<*>
+    private lateinit var recyclerViewManager: RecyclerView.LayoutManager
+    private lateinit var mainMenuCards: Array<MainMenuCard>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+
+        recyclerViewManager = LinearLayoutManager(this)
+        recyclerViewAdapter = MainMenuAdapter(mainMenuCards)
+
+        // TODO: Change ID
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = recyclerViewManager
+            adapter = recyclerViewAdapter
+        }
+    }
+
+}
