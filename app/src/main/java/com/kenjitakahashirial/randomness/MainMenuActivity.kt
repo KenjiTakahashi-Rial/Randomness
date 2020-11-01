92438928531ad64a9a4b657fbd4894882fd551d9
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-10-31 19:12:17 -0700

    Shortened MainMenuActivity

--- app/src/main/java/com/kenjitakahashirial/randomness/MainMenuActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/MainMenuActivity.kt
@@ -6,27 +6,18 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainMenuActivity : AppCompatActivity() {
-    private lateinit var layoutManager: LinearLayoutManager
-    private lateinit var cards: Array<MainMenuCard>
-    private lateinit var adapter: MainMenuAdapter
-    private lateinit var recyclerView: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
-        layoutManager = LinearLayoutManager(this)
-        cards = initializeCards()
-        adapter = MainMenuAdapter(cards)
-
-        recyclerView = findViewById<RecyclerView>(R.id.main_menu_recycler_view).apply {
+        findViewById<RecyclerView>(R.id.main_menu_recycler_view).apply {
             setHasFixedSize(true)
-            layoutManager = this@MainMenuActivity.layoutManager
-            adapter = this@MainMenuActivity.adapter
+            layoutManager = LinearLayoutManager(this@MainMenuActivity)
+            adapter = MainMenuAdapter(createCards())
         }
     }
 
-    private fun initializeCards(): Array<MainMenuCard> = arrayOf(
+    private fun createCards(): Array<MainMenuCard> = arrayOf(
         MainMenuCard("temp_name0", R.drawable.temp_rando, AppCompatActivity()),
         MainMenuCard("temp_name1", R.drawable.temp_rando, AppCompatActivity()),
         MainMenuCard("temp_name2", R.drawable.temp_rando, AppCompatActivity()),
