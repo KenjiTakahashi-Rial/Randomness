180c254ea397a5200bacefec501abfcaeaf8e75b
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-10-31 19:01:41 -0700

    Cleaned up MainMenuActivity

--- app/src/main/java/com/kenjitakahashirial/randomness/MainMenuActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/MainMenuActivity.kt
@@ -6,28 +6,33 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainMenuActivity : AppCompatActivity() {
-    private lateinit var mainMenuCards: Array<MainMenuCard>
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var cards: Array<MainMenuCard>
+    private lateinit var adapter: MainMenuAdapter
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
-        mainMenuCards = arrayOf(
-            MainMenuCard("temp_name0", R.drawable.temp_rando, AppCompatActivity()),
-            MainMenuCard("temp_name1", R.drawable.temp_rando, AppCompatActivity()),
-            MainMenuCard("temp_name2", R.drawable.temp_rando, AppCompatActivity()),
-            MainMenuCard("temp_name3", R.drawable.temp_rando, AppCompatActivity()),
-            MainMenuCard("temp_name4", R.drawable.temp_rando, AppCompatActivity()),
-            MainMenuCard("temp_name5", R.drawable.temp_rando, AppCompatActivity()),
-            MainMenuCard("temp_name6", R.drawable.temp_rando, AppCompatActivity())
-        )
+        layoutManager = LinearLayoutManager(this)
+        cards = initializeCards()
+        adapter = MainMenuAdapter(cards)
 
         recyclerView = findViewById<RecyclerView>(R.id.main_menu_recycler_view).apply {
             setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(this@MainMenuActivity)
-            adapter = MainMenuAdapter(mainMenuCards)
+            layoutManager = this@MainMenuActivity.layoutManager
+            adapter = this@MainMenuActivity.adapter
         }
     }
 
+    private fun initializeCards(): Array<MainMenuCard> = arrayOf(
+        MainMenuCard("temp_name0", R.drawable.temp_rando, AppCompatActivity()),
+        MainMenuCard("temp_name1", R.drawable.temp_rando, AppCompatActivity()),
+        MainMenuCard("temp_name2", R.drawable.temp_rando, AppCompatActivity()),
+        MainMenuCard("temp_name3", R.drawable.temp_rando, AppCompatActivity()),
+        MainMenuCard("temp_name4", R.drawable.temp_rando, AppCompatActivity()),
+        MainMenuCard("temp_name5", R.drawable.temp_rando, AppCompatActivity()),
+        MainMenuCard("temp_name6", R.drawable.temp_rando, AppCompatActivity())
+    )
 }
