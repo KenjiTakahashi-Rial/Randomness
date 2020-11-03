60681311f6946a555913fba2552da0e3dc79265c
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-02 22:14:26 -0800

    Created placeholder FlipCoinSettingsActivity

---
+++ app/src/main/java/com/kenjitakahashirial/randomness/FlipCoinSettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.kenjitakahashirial.randomness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlipCoinSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flip_coin_settings)
+    }
+}
