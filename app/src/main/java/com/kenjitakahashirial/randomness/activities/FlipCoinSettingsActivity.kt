f00e5907d25da078938bc618f6094641f76819d8
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-02 22:29:28 -0800

    Filled in FlipCoinSettingsActivity properties

--- app/src/main/java/com/kenjitakahashirial/randomness/activities/FlipCoinSettingsActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/FlipCoinSettingsActivity.kt
@@ -1,12 +1,16 @@
 package com.kenjitakahashirial.randomness.activities
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.kenjitakahashirial.randomness.R
+import com.kenjitakahashirial.randomness.activities.abstract.BaseRandomSettingsActivity
 
-class FlipCoinSettingsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_flip_coin_settings)
+class FlipCoinSettingsActivity : BaseRandomSettingsActivity() {
+    override val layout = R.layout.activity_flip_coin_settings
+    override val layoutId = R.id.flipCoinSettingsLayout
+    override val saveButtonId = R.id.flipCoinSettingsSaveButton
+    override val cancelButtonId = R.id.flipCoinSettingsCancelButton
+    override val settingsId = R.string.flip_coin_settings_key
+
+    override fun save() {
+        TODO("Not yet implemented")
     }
 }
