7e7bb663373d908e3b8f9724221d8489e5538724
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-03 21:10:56 -0800

    Made FlipCoinSettings save

--- app/src/main/java/com/kenjitakahashirial/randomness/activities/FlipCoinSettingsActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/FlipCoinSettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.kenjitakahashirial.randomness.activities
 
+import android.os.Bundle
+import android.widget.EditText
 import com.kenjitakahashirial.randomness.R
 import com.kenjitakahashirial.randomness.activities.abstract.BaseRandomSettingsActivity
+import com.kenjitakahashirial.randomness.utilities.FlipCoinSettings
 
 class FlipCoinSettingsActivity : BaseRandomSettingsActivity() {
     override val layout = R.layout.activity_flip_coin_settings
@@ -10,7 +13,31 @@ class FlipCoinSettingsActivity : BaseRandomSettingsActivity() {
     override val cancelButtonId = R.id.flipCoinSettingsCancelButton
     override val settingsId = R.string.flip_coin_settings_key
 
-    override fun save() {
-        TODO("Not yet implemented")
+    private lateinit var numCoinsText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val settings = sharedPreferences.getClass(settingsKey, FlipCoinSettings())
+
+        numCoinsText = findViewById(R.id.flipCoinSettingsNumCoins)
+        numCoinsText.setText(settings.numCoins.toString())
+    }
+
+    override fun getSettings(): Pair<FlipCoinSettings, SettingsError>  {
+        var isValidInt = true
+        val settings = FlipCoinSettings()
+
+        try {
+            settings.numCoins = numCoinsText.text.toString().toInt()
+        } catch (e: NumberFormatException) {
+            isValidInt = false
+        }
+
+        val error =
+            if (!isValidInt || settings.numCoins !in 1..10) SettingsError.NUMBER
+            else SettingsError.NONE
+
+        return Pair(settings, error)
     }
 }
