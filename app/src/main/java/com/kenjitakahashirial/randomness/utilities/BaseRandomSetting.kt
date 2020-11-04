21e954b5af1e2443721d67a325c8c3838db7cacd
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-03 21:05:33 -0800

    Missed commit

---
+++ app/src/main/java/com/kenjitakahashirial/randomness/utilities/BaseRandomSetting.kt
@@ -0,0 +1,23 @@
+package com.kenjitakahashirial.randomness.utilities
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
+
+@Serializable
+abstract class BaseRandomSettings
+
+val baseRandomSettingsSerializerModule = SerializersModule {
+    polymorphic(BaseRandomSettings::class) {
+        subclass(RandomIntegerSettings::class)
+        subclass(FlipCoinSettings::class)
+    }
+}
+
+val baseRandomSettingsSerializerFormat = Json {
+    encodeDefaults = true
+    ignoreUnknownKeys = true
+    serializersModule = baseRandomSettingsSerializerModule
+}
