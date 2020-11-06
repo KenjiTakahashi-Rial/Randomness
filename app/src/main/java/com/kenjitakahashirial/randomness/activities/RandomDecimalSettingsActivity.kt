a77bc968058813e28926e2c4f6f01b2a8aa550a1
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-06 13:24:59 -0800

    Filled in RandomDecimalSettingsActivity

--- app/src/main/java/com/kenjitakahashirial/randomness/activities/RandomDecimalSettingsActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/RandomDecimalSettingsActivity.kt
@@ -1,28 +1,58 @@
 package com.kenjitakahashirial.randomness.activities
 
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.widget.SwitchCompat
 import com.kenjitakahashirial.randomness.R
 import com.kenjitakahashirial.randomness.activities.abstract.BaseRandomSettingsActivity
 import com.kenjitakahashirial.randomness.utilities.BaseRandomSettings
+import com.kenjitakahashirial.randomness.utilities.RandomDecimalSettings
 
 class RandomDecimalSettingsActivity : BaseRandomSettingsActivity() {
-    override val layout: Int
-        get() = TODO("Not yet implemented")
-    override val layoutId: Int
-        get() = TODO("Not yet implemented")
-    override val saveButtonId: Int
-        get() = TODO("Not yet implemented")
-    override val cancelButtonId: Int
-        get() = TODO("Not yet implemented")
-    override val settingsId: Int
-        get() = TODO("Not yet implemented")
+    override val layout = R.layout.activity_random_decimal_settings
+    override val layoutId = R.id.randomDecimalSettingsLayout
+    override val saveButtonId = R.id.randomDecimalSettingsSaveButton
+    override val cancelButtonId = R.id.randomDecimalSettingsCancelButton
+    override val settingsId = R.string.random_decimal_settings_key
+
+    private val decimalPlacesRange = 1..8
+    private lateinit var decimalPlacesText: EditText
+    private lateinit var showTrailingZerosSwitch: SwitchCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_random_decimal_settings)
+
+        val settings = sharedPreferences.getClass(settingsKey, RandomDecimalSettings())
+
+        decimalPlacesText = findViewById(R.id.randomDecimalSettingsPlaces)
+        showTrailingZerosSwitch = findViewById(R.id.randomDecimalSettingsShowTrailingZeros)
+
+        with (settings) {
+            decimalPlacesText.setText(decimalPlaces.toString())
+            showTrailingZerosSwitch.isChecked = showTrailingZeros
+        }
+
+        decimalPlacesText.hint = with (decimalPlacesRange) { getString(R.string.range_format, first, last) }
     }
 
     override fun getSettings(): Pair<BaseRandomSettings, SettingsError> {
-        TODO("Not yet implemented")
+        var isValidInt = true
+        val settings = RandomDecimalSettings()
+
+        try {
+            settings.apply {
+                decimalPlaces = decimalPlacesText.text.toString().toInt()
+                showTrailingZeros = showTrailingZerosSwitch.isChecked
+            }
+        } catch (e: NumberFormatException) {
+            isValidInt = false
+        }
+
+        val error = with (settings) {
+            if (!isValidInt || decimalPlaces !in decimalPlacesRange) SettingsError.RANGE
+            else SettingsError.NONE
+        }
+
+        return Pair(settings, error)
     }
 }
