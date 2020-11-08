f5bcb9c866927b3f2304aa7454bb9096a39de874
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-07 21:08:22 -0800

    Created placeholder random word activities

---
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/RandomWordActivity.kt
@@ -0,0 +1,12 @@
+package com.kenjitakahashirial.randomness.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kenjitakahashirial.randomness.R
+
+class RandomWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_word)
+    }
+}
