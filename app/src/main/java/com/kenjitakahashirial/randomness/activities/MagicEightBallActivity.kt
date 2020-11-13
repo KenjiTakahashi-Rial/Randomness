a8805f5346bc1a90c45744e2daa38a633ada546d
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-13 15:17:24 -0800

    Created placeholder magic 8 ball activities

---
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/MagicEightBallActivity.kt
@@ -0,0 +1,23 @@
+package com.kenjitakahashirial.randomness.activities
+
+import com.kenjitakahashirial.randomness.activities.abstract.BaseRandomActivity
+import com.kenjitakahashirial.randomness.activities.abstract.BaseRandomSettingsActivity
+
+class MagicEightBallActivity : BaseRandomActivity() {
+    override val layout: Int
+        get() = TODO("Not yet implemented")
+    override val resultTextViewId: Int
+        get() = TODO("Not yet implemented")
+    override val generateButtonId: Int
+        get() = TODO("Not yet implemented")
+    override val settingsButtonId: Int
+        get() = TODO("Not yet implemented")
+    override val settingsId: Int
+        get() = TODO("Not yet implemented")
+    override val settingsActivityClass: Class<out BaseRandomSettingsActivity>
+        get() = TODO("Not yet implemented")
+
+    override fun generateNext() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/MagicEightBallSettingsActivity.kt
@@ -0,0 +1,28 @@
+package com.kenjitakahashirial.randomness.activities
+
+import android.os.Bundle
+import com.kenjitakahashirial.randomness.R
+import com.kenjitakahashirial.randomness.activities.abstract.BaseRandomSettingsActivity
+import com.kenjitakahashirial.randomness.utilities.BaseRandomSettings
+
+class MagicEightBallSettingsActivity : BaseRandomSettingsActivity() {
+    override val layout: Int
+        get() = TODO("Not yet implemented")
+    override val layoutId: Int
+        get() = TODO("Not yet implemented")
+    override val saveButtonId: Int
+        get() = TODO("Not yet implemented")
+    override val cancelButtonId: Int
+        get() = TODO("Not yet implemented")
+    override val settingsId: Int
+        get() = TODO("Not yet implemented")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_magic_eight_ball_settings)
+    }
+
+    override fun getSettings(): Pair<BaseRandomSettings, SettingsError> {
+        TODO("Not yet implemented")
+    }
+}
