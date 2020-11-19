8b1943d0f14012f390b5d47e863664b66b007a72
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-19 11:08:58 -0800

    Added programmatically inflated images for flip coin results

--- app/src/main/java/com/kenjitakahashirial/randomness/activities/FlipCoinActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/FlipCoinActivity.kt
@@ -1,8 +1,10 @@
 package com.kenjitakahashirial.randomness.activities
 
+import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.get
 import androidx.core.view.iterator
 import com.kenjitakahashirial.randomness.R
 import com.kenjitakahashirial.randomness.activities.abstract.BaseRandomActivity
@@ -17,17 +19,31 @@ class FlipCoinActivity : BaseRandomActivity() {
     override val settingsId = R.string.flip_coin_settings_key
     override val settingsActivityClass = FlipCoinSettingsActivity::class.java
 
+    private val maxResultsPerRow = 5
+    private val resultImageViews = mutableListOf<ImageView>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        inflateResultViews()
+    }
+
     override fun generateNext() {
         val settings = sharedPreferences.getClass(settingsKey, FlipCoinSettings())
-        var coinIndex = 0
 
-        for (horizontalListView in (resultView as LinearLayoutCompat)) {
-            for (imageView in (horizontalListView as LinearLayoutCompat)) {
-                (imageView as ImageView).apply {
-                    if (coinIndex < settings.numCoins) generateNextCoinImage()
-                    else visibility = View.GONE
-                }
-                coinIndex++
+        for ((index, result) in resultImageViews.withIndex()) {
+            result.apply {
+                generateNextCoinImage()
+                visibility = if (index < settings.numCoins) View.VISIBLE else View.GONE
+            }
+        }
+    }
+
+    private fun inflateResultViews() {
+        for (resultRowParent in resultView as ViewGroup) {
+            for (i in 0 until maxResultsPerRow) {
+                layoutInflater.inflate(R.layout.view_flip_coin_result, resultRowParent as ViewGroup)
+                resultImageViews.add(resultRowParent[i] as ImageView)
+                resultImageViews.last().visibility = View.GONE
             }
         }
     }
@@ -37,6 +53,5 @@ class FlipCoinActivity : BaseRandomActivity() {
             if (Random.nextBoolean()) R.mipmap.american_silver_eagle_obverse
             else R.mipmap.american_silver_eagle_reverse
         )
-        visibility = View.VISIBLE
     }
 }
