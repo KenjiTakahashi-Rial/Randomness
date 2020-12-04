4c1e52574ec0fb911751a748ee1fd9c25a2129c8
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-12-04 11:48:47 -0800

    Added AutoSizeTextView

---
+++ app/src/main/java/com/kenjitakahashirial/randomness/utilities/AutoSizeTextView.kt
@@ -0,0 +1,20 @@
+package com.kenjitakahashirial.randomness.utilities
+
+import android.content.Context
+import android.util.AttributeSet
+
+class AutoSizeTextView(context: Context, attrs: AttributeSet) : AutoSizeEditText(context, attrs) {
+    init {
+        disableEdits()
+    }
+
+    private fun disableEdits() {
+        isCursorVisible = false
+        isLongClickable = false
+        isClickable = false
+        isFocusable = false
+        isSelected = false
+        keyListener = null
+        setBackgroundResource(android.R.color.transparent)
+    }
+}
