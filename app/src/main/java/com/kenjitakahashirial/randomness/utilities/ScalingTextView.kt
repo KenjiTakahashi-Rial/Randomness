e70d2f7700d910a22ea23023558b28b218e7f867
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-12-09 16:57:50 -0800

    Added ScalingTextView

---
+++ app/src/main/java/com/kenjitakahashirial/randomness/utilities/ScalingTextView.kt
@@ -0,0 +1,29 @@
+package com.kenjitakahashirial.randomness.utilities
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.util.DisplayMetrics
+import androidx.appcompat.widget.AppCompatTextView
+
+class ScalingTextView(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
+    private val textSizeWidthRatio: Float
+    private val textSizeHeightRatio: Float
+
+    init {
+        val displayMetrics = DisplayMetrics()
+        (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
+        textSizeWidthRatio = textSize / displayMetrics.widthPixels
+        textSizeHeightRatio = textSize / displayMetrics.heightPixels
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        scaleTextSize()
+    }
+
+    private fun scaleTextSize() {
+        textSize = minOf(textSizeWidthRatio * width, textSizeHeightRatio * height)
+    }
+}
