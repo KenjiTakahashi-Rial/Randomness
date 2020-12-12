b7ea8c07ec855b205bc8d93b264e484a4fe8c654
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-12-12 12:18:33 -0800

    Hide the Random Color copy button before any results are shown

--- app/src/main/java/com/kenjitakahashirial/randomness/activities/RandomColorActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/RandomColorActivity.kt
@@ -4,6 +4,8 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import com.kenjitakahashirial.randomness.R
@@ -19,6 +21,7 @@ class RandomColorActivity : BaseRandomActivity() {
     override val settingsActivityClass = RandomColorSettingsActivity::class.java
 
     private lateinit var resultTextView: TextView
+    private lateinit var copyParent: ViewGroup
     private lateinit var copyButton: Button
 
     private val settings: RandomColorSettings
@@ -28,6 +31,9 @@ class RandomColorActivity : BaseRandomActivity() {
         super.onCreate(savedInstanceState)
 
         resultTextView = findViewById(R.id.randomColorResultTextView)
+        copyParent = findViewById<ViewGroup>(R.id.randomColorCopyParent).apply {
+            visibility = View.INVISIBLE
+        }
         copyButton = findViewById<Button>(R.id.randomColorCopyButton).apply {
             setOnClickListener { copyHex() }
         }
@@ -40,6 +46,7 @@ class RandomColorActivity : BaseRandomActivity() {
 
         baseResultView.setBackgroundColor(result)
         resultTextView.text = getString(R.string.format_hex, result.toColorHexString())
+        copyParent.visibility = View.VISIBLE
     }
 
     private fun copyHex() {
