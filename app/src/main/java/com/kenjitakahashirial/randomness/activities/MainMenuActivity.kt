417fb4ee6d4711ad79b9828c94cfd6904fc2ddc9
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-12-12 14:35:17 -0800

    Missed commit

--- app/src/main/java/com/kenjitakahashirial/randomness/activities/MainMenuActivity.kt
+++ app/src/main/java/com/kenjitakahashirial/randomness/activities/MainMenuActivity.kt
@@ -35,13 +35,13 @@ class MainMenuActivity : AppCompatActivity() {
     }
 
     private fun createCards(): Array<MainMenuCard> = arrayOf(
-        MainMenuCard(getString(R.string.random_integer_name), R.mipmap.main_menu_image_random_integer, RandomIntegerActivity::class.java),
-        MainMenuCard(getString(R.string.flip_coin_name), R.mipmap.main_menu_image_flip_coin, FlipCoinActivity::class.java),
-        MainMenuCard(getString(R.string.roll_dice_name), R.mipmap.main_menu_image_roll_dice, RollDiceActivity::class.java),
-        MainMenuCard(getString(R.string.random_decimal_name), R.mipmap.main_menu_image_random_decimal, RandomDecimalActivity::class.java),
-        MainMenuCard(getString(R.string.random_word_name), R.mipmap.main_menu_image_random_word, RandomWordActivity::class.java),
-        MainMenuCard(getString(R.string.random_color_name), R.mipmap.main_menu_image_random_color, RandomColorActivity::class.java),
-        MainMenuCard(getString(R.string.magic_eight_ball_name), R.mipmap.main_menu_image_magic_eight_ball, MagicEightBallActivity::class.java)
+        MainMenuCard(getString(R.string.random_integer_name), R.mipmap.temp_rando, RandomIntegerActivity::class.java),
+        MainMenuCard(getString(R.string.flip_coin_name), R.mipmap.temp_rando, FlipCoinActivity::class.java),
+        MainMenuCard(getString(R.string.roll_dice_name), R.mipmap.temp_rando, RollDiceActivity::class.java),
+        MainMenuCard(getString(R.string.random_decimal_name), R.mipmap.temp_rando, RandomDecimalActivity::class.java),
+        MainMenuCard(getString(R.string.random_word_name), R.mipmap.temp_rando, RandomWordActivity::class.java),
+        MainMenuCard(getString(R.string.random_color_name), R.mipmap.temp_rando, RandomColorActivity::class.java),
+        MainMenuCard(getString(R.string.magic_eight_ball_name), R.mipmap.temp_rando, MagicEightBallActivity::class.java)
     )
 
     private fun getColorRange(startId: Int, endId: Int): IntRange =
